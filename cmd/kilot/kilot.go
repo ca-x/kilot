c1cedd1deabb1b1353d7562ebee7ec638be96352
czyt <czyt@w.cn>
2022-09-13 11:42:53 +0800

    change version

--- cmd/kilot/kilot.go
+++ cmd/kilot/kilot.go
@@ -22,7 +22,7 @@ func main() {
 			},
 		},
 		Name:                 "kilot",
-		Version:              "0.1.5",
+		Version:              "0.1.6",
 		EnableBashCompletion: true,
 		Commands: []*cli.Command{
 			{
