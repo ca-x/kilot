ce5d9209863c8f2d26d3f63fb0475b8e8feec7a2
虫子樱桃 <czyt.go@gmail.com>
2023-09-22 13:05:17 +0800

    Update kilot.go

--- cmd/kilot/kilot.go
+++ cmd/kilot/kilot.go
@@ -22,7 +22,7 @@ func main() {
 			},
 		},
 		Name:                 "kilot",
-		Version:              "0.1.16",
+		Version:              "0.1.17",
 		EnableBashCompletion: true,
 		Commands: []*cli.Command{
 			{
